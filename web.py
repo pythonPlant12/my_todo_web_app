231fc73e0ef3806f177f7af76d15bd19d2a303e5
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-11 08:57:36 +0100

    Added requirements.txt and .gitignore

--- web.py
+++ web.py
@@ -18,6 +18,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Enter a todo", on_change=add_todo,
+st.text_input(label=" ", placeholder="Enter a todo", on_change=add_todo,
               key="new_todo")
 
